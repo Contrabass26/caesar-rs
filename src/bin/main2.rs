d58344e37b7002273a899f34593693d307f44ac8
Contrabass26 <jsedwards315@outlook.com>
2026-07-05 19:44:30 +0100

    Streamline recursion and improve logging

--- src/bin/main2.rs
+++ src/bin/main2.rs
@@ -1,5 +1,6 @@
-use std::collections::{HashMap};
-use std::process::exit;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
 
 macro_rules! paint {
     ($s:expr) => {
@@ -207,108 +208,113 @@ struct Exploration {
     is_placed: u64, // ...with pieces that hadn't been placed according to `is_placed`
 }
 
-// Returns the number of ways in which all of these regions can be filled with the pieces available
-fn fill(regions: [u64; MAX_REGIONS], num_regions: usize, placements: &mut [u64; NUM_PIECES], is_placed: u64, num_placed: usize, cache: &mut HashMap<Exploration, u64>, flag: bool) -> u64 {
-    // Returns the number of ways in which regions[region_index..num_regions) can be filled with the pieces available
-    fn fill_region(regions: &[u64; MAX_REGIONS], num_regions: usize, region_index: usize, placements: &mut [u64; NUM_PIECES], is_placed: u64, num_placed: usize, cache: &mut HashMap<Exploration, u64>, flag: bool) -> u64 {
-        let spaces = regions[region_index];
-        // Check whether this state has been explored already
-        if let Some(n) = cache.get(&Exploration { region: spaces, is_placed }) {
-            return *n;
-        }
-        // Keep track of the number of ways we've found
-        let mut num_ways = 0;
-        // Choose a piece to place
-        for piece_index in 0..NUM_ALL_PIECES {
-            if is_placed & (1 << piece_index) != 0 { continue }
-            let base_piece = PIECES[piece_index];
-            // Choose a location for it
-            let mut y_base = base_piece;
-            // We don't actually need to try the bottom row: no piece can start there
-            for _y in 0..=6 {
-                let mut piece = y_base;
-                loop {
-                    if piece | spaces == spaces {
-                        // There is room for the piece here
-                        placements[num_placed] = piece;
-                        if flag { println!("Placed first piece {}", piece) }
-                        if spaces & !piece == 0 {
-                            // This region has been filled
-                            if region_index + 1 >= num_regions {
-                                // All regions have been filled
-                                num_ways += 1;
-                            } else {
-                                // Fill the next one
-                                num_ways += fill_region(regions, num_regions, region_index + 1, placements, is_placed | PIECE_FAMILIES[piece_index], num_placed + 1, cache, false);
+// Returns the number of ways in which regions[region_index..num_regions) can be filled with the pieces available
+fn fill(regions: [u64; MAX_REGIONS], num_regions: usize, region_index: usize, placements: &mut [u64; NUM_PIECES], is_placed: u64, num_placed: usize, cache: &mut HashMap<Exploration, u128>, flag: bool, file: &mut File) -> u128 {
+    let spaces = regions[region_index];
+    // Check whether this state has been explored already
+    if let Some(n) = cache.get(&Exploration { region: spaces, is_placed }) {
+        return *n;
+    }
+    // Keep track of the number of ways we've found
+    let mut num_ways = 0;
+    // Choose a piece to place
+    for piece_index in 0..NUM_ALL_PIECES {
+        if is_placed & (1 << piece_index) != 0 { continue }
+        let base_piece = PIECES[piece_index];
+        // Choose a location for it
+        let mut y_base = base_piece;
+        // We don't actually need to try the bottom row: no piece can start there
+        for _y in 0..=6 {
+            let mut piece = y_base;
+            loop {
+                if piece | spaces == spaces {
+                    // There is room for the piece here
+                    placements[num_placed] = piece;
+                    if flag { println!("Placed first piece {}", piece) }
+                    if spaces & !piece == 0 {
+                        // This region has been filled
+                        if region_index + 1 >= num_regions {
+                            // All regions have been filled
+                            num_ways += 1;
+                            if num_placed == 9 {
+                                let mut message = String::from("Found solution!");
+                                for j in 0..num_placed + 1 {
+                                    message = message + &" " + placements[j].to_string().as_str();
+                                }
+                                message += &"\n";
+                                print!("{}", message);
+                                file.write_all(message.as_bytes()).expect("Failed to write to file");
                             }
                         } else {
-                            // Split the grid into regions
-                            let mut new_regions = [0u64; MAX_REGIONS];
-                            let mut region_sizes = [i32::MAX; MAX_REGIONS];
-                            let mut new_num_regions = 0;
-                            {
-                                let mut covered = !spaces | piece; // the cells that can't be added to a new region
-                                let mut start = 1;
-                                let mut i = 0; // start = 2^i
-                                'find_region: loop {
-                                    // Find a cell which isn't in a region yet
-                                    while covered & start != 0 {
-                                        i += 1;
-                                        start <<= 1;
-                                        if i > 55 { break 'find_region; } // we've checked all the cells and they don't need a region
-                                    }
-                                    // Bucket-fill a region starting from here
-                                    let mut region = start;
-                                    covered |= start;
-                                    let mut size = 1;
-                                    {
-                                        let mut changed = false;
-                                        let mut candidate = 1;
-                                        let mut forward = true;
-                                        loop {
-                                            if candidate & covered == 0 && ((candidate >> 7) & region != 0 || (candidate << 7) & region != 0 || candidate & FIRST_COL == 0 && (candidate >> 1) & region != 0 || candidate & LAST_COL == 0 && (candidate << 1) & region != 0) {
-                                                region |= candidate;
-                                                covered |= candidate;
-                                                changed = true;
-                                                size += 1;
-                                            }
-                                            if candidate == 1 && !forward || candidate == 1 << 55 && forward {
-                                                forward = !forward;
-                                                if !changed { break }
-                                                changed = false;
-                                            }
-                                            candidate = if forward { candidate << 1 } else { candidate >> 1 }
-                                        }
-                                    }
-                                    // Add this region to the array
-                                    if new_num_regions >= MAX_REGIONS {
-                                        panic!("Region array isn't big enough")
-                                    }
-                                    new_regions[new_num_regions] = region;
-                                    region_sizes[new_num_regions] = size;
-                                    new_num_regions += 1;
-                                }
-                            }
-                            // Sort the regions by size
-                            let mut indices: [usize; MAX_REGIONS] = core::array::from_fn(|i| i);
-                            indices.sort_by_key(|i| region_sizes[*i]);
-                            // Fill all these regions
-                            let sorted_regions: [u64; MAX_REGIONS] = core::array::from_fn(|i| new_regions[indices[i]]);
-                            num_ways += fill(sorted_regions, new_num_regions, placements, is_placed | PIECE_FAMILIES[piece_index], num_placed + 1, cache, false);
+                            // Fill the next one
+                            num_ways += fill(regions, num_regions, region_index + 1, placements, is_placed | PIECE_FAMILIES[piece_index], num_placed + 1, cache, false, file);
                         }
+                    } else {
+                        // Split the grid into regions
+                        let mut new_regions = [0u64; MAX_REGIONS];
+                        let mut region_sizes = [i32::MAX; MAX_REGIONS];
+                        let mut new_num_regions = 0;
+                        {
+                            let mut covered = !spaces | piece; // the cells that can't be added to a new region
+                            let mut start = 1;
+                            let mut i = 0; // start = 2^i
+                            'find_region: loop {
+                                // Find a cell which isn't in a region yet
+                                while covered & start != 0 {
+                                    i += 1;
+                                    start <<= 1;
+                                    if i > 55 { break 'find_region; } // we've checked all the cells and they don't need a region
+                                }
+                                // Bucket-fill a region starting from here
+                                // TODO: Better recursion-based (DFS) bucket fill?
+                                let mut region = start;
+                                covered |= start;
+                                let mut size = 1;
+                                {
+                                    let mut changed = false;
+                                    let mut candidate = 1;
+                                    let mut forward = true;
+                                    loop {
+                                        if candidate & covered == 0 && ((candidate >> 7) & region != 0 || (candidate << 7) & region != 0 || candidate & FIRST_COL == 0 && (candidate >> 1) & region != 0 || candidate & LAST_COL == 0 && (candidate << 1) & region != 0) {
+                                            region |= candidate;
+                                            covered |= candidate;
+                                            changed = true;
+                                            size += 1;
+                                        }
+                                        if candidate == 1 && !forward || candidate == 1 << 55 && forward {
+                                            forward = !forward;
+                                            if !changed { break }
+                                            changed = false;
+                                        }
+                                        candidate = if forward { candidate << 1 } else { candidate >> 1 }
+                                    }
+                                }
+                                // Add this region to the array
+                                if new_num_regions >= MAX_REGIONS {
+                                    panic!("Region array isn't big enough")
+                                }
+                                new_regions[new_num_regions] = region;
+                                region_sizes[new_num_regions] = size;
+                                new_num_regions += 1;
+                            }
+                        }
+                        // Sort the regions by size
+                        let mut indices: [usize; MAX_REGIONS] = core::array::from_fn(|i| i);
+                        indices.sort_by_key(|i| region_sizes[*i]);
+                        // Fill all these regions
+                        let sorted_regions: [u64; MAX_REGIONS] = core::array::from_fn(|i| new_regions[indices[i]]);
+                        num_ways += fill(sorted_regions, new_num_regions, 0, placements, is_placed | PIECE_FAMILIES[piece_index], num_placed + 1, cache, false, file);
                     }
-                    piece <<= 1;
-                    if piece & FIRST_COL != 0 { break }
                 }
-                y_base <<= 7;
+                piece <<= 1;
+                if piece & FIRST_COL != 0 { break }
             }
+            y_base <<= 7;
         }
-        // Write to the cache
-        cache.insert(Exploration { region: spaces, is_placed }, num_ways);
-        num_ways
     }
-
-    fill_region(&regions, num_regions, 0, placements, is_placed, num_placed, cache, flag)
+    // Write to the cache
+    cache.insert(Exploration { region: spaces, is_placed }, num_ways);
+    num_ways
 }
 
 fn main() {
@@ -316,6 +322,7 @@ fn main() {
     regions[0] = TO_FILL;
     let mut placements = [0u64; NUM_PIECES];
     let mut cache = HashMap::new();
-    let num_ways = fill(regions, 1, &mut placements, 0, 0, &mut cache, true);
+    let mut file = File::create("log.txt").expect("Failed to create File");
+    let num_ways = fill(regions, 1, 0, &mut placements, 0, 0, &mut cache, true, &mut file);
     println!("num_ways = {}", num_ways);
 }
