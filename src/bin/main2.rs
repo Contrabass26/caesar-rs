dc902c5450d2cb79bf8c187d8d085affa9919aaa
Contrabass26 <jsedwards315@outlook.com>
2026-07-09 13:53:03 +0100

    Change log to CSV

--- src/bin/main2.rs
+++ src/bin/main2.rs
@@ -346,10 +346,10 @@ fn count_all() {
     });
 
     let collector_handle = thread::spawn(move || {
-        let mut file = File::create("log.txt").expect("Failed to create log file");
+        let mut file = File::create("log.csv").expect("Failed to create log file");
         for _input in 0..NUM_PUZZLES {
             let result = rx.recv().expect("Failed to receive result");
-            let message = format!("{}-{}-{} = {}\n", result.i, result.j, result.k, result.num_ways);
+            let message = format!("{},{},{},{}\n", result.i, result.j, result.k, result.num_ways);
             print!("{}", message);
             file.write(message.as_bytes()).expect("Failed to write to file");
         }
